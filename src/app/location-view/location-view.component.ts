15d5e07d335a25ebaf1a6182f2ecc49a16bb0bfa
mdsecurity <mattiasduffy@MacBook-Woah.local>
2016-12-27 10:33:45 -0500

    still dont know how to stash

--- src/app/location-view/location-view.component.ts
+++ src/app/location-view/location-view.component.ts
@@ -35,8 +35,8 @@ export class LocationViewComponent implements OnInit {
     let picture = '../assets/JC.jpg';
     
     this.locations[0] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
-    // this.locations[1] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
-    // this.locations[2] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
+    this.locations[1] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
+    this.locations[2] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
     // this.locations[3] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
     // this.locations[4] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
     // this.locations[5] = new Location('Easy Eats', openTimes, closeTimes, picture, 'open');
