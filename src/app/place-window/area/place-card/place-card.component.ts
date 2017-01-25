4a5939599e6e207187aa54d3a482e7e02f94ada8
mdsecurity <mattiasduffy@MacBook-Woah.local>
2017-01-24 22:02:14 -0500

    Started working on the typescript for places

--- src/app/place-window/area/place-card/place-card.component.ts
+++ src/app/place-window/area/place-card/place-card.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Place } from '../../../place';
 @Component({
   selector: 'app-place-card',
   templateUrl: './place-card.component.html',
   styleUrls: ['./place-card.component.scss']
 })
 export class PlaceCardComponent implements OnInit {
-
+  @Input() private place: Place;
+  private status: string;
   constructor() { }
 
   ngOnInit() {
+    this.place = new Place('Burger King', [new Date()],
+      [new Date()], 'picture: string', 'open', 'The Johnson Center');
+
+    if (this.place.status === 'open') {
+      status = 'OPEN';
+    } else {
+      status = 'CLOSED';
+    }
+
   }
 
 }
--- src/app/place.ts
+++ src/app/place.ts
@@ -5,14 +5,17 @@ export class Place {
     status: string;
     startTimes: Date[];
     endTimes: Date[];
+    location: string;
     constructor(name: string, startTimes: Date[],
-        endTimes: Date[], picture: string, status: string) {
+        endTimes: Date[], picture: string, status: string, location: string) {
 
         this.name = name;
         this.startTimes = startTimes;
         this.endTimes = endTimes;
         this.picture = picture;
         this.status = status;
+        this.location = location;
+
     }
 }
 
