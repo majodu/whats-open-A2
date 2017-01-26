7d78bf5b8eb213d434777b7f4b59076ce2cc431a
mdsecurity <mattiasduffy@MacBook-Woah.local>
2017-01-25 21:05:31 -0500

    adding to place card ts

--- src/app/place-window/area/place-card/place-card.component.ts
+++ src/app/place-window/area/place-card/place-card.component.ts
@@ -8,16 +8,36 @@ import { Place } from '../../../place';
 export class PlaceCardComponent implements OnInit {
   @Input() private place: Place;
   private status: string;
+  private week: string[] = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
   constructor() { }
 
   ngOnInit() {
-    this.place = new Place('Burger King', [new Date()],
-      [new Date()], 'picture: string', 'open', 'The Johnson Center');
+    let t1 = new Date();
+    let t2 = new Date();
+    let weekStartTimes: Date[] = [];
+    let weekEndTimes: Date[] = [];
+
+    for (let i = 0; i < 7; i++) {
+      t1 = new Date();
+      t2 = new Date();
+      t1.setDate(5 + Math.random() * 6);
+      t1.setHours((Math.random() * 10) + 1);
+      t2.setHours((Math.random() * 10) + 13);
+
+      weekStartTimes.push(t1);
+      weekEndTimes.push(t2);
+    }
+    this.place = new Place('Burger King', weekStartTimes,
+      weekEndTimes, 'picture: string', 'open', 'The Johnson Center');
+
+    let todaysDate = this.place[new Date().getDay()];
+    console.log(todaysDate);
 
     if (this.place.status === 'open') {
-      status = 'OPEN';
+      this.status = 'OPEN';
     } else {
-      status = 'CLOSED';
+      this.status = 'CLOSED';
     }
 
   }
