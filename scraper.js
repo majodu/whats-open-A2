10c5c55c89a7ae5a51a9c1936e4f85dd4f274e3a
mdsecurity <mattiasduffy@MacBook-Woah.local>
2017-02-01 23:38:19 -0500

    adding scraper and fixing deps dont look at the scraper now its dog shit

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 var Xray = require('x-ray');
 var fs = require('fs');
+var XRegExp = require('xregexp');
 var x = Xray({
     filters: {
         trim: function (value) {
@@ -23,15 +24,16 @@ var x = Xray({
             value = value.split(' ').join('');
             value = value.split(',');
             for(let i = 0; i < value.length;i++){
-                
+                value[i] = value[i].replace('a',',a')
+                value[i] = value[i].replace('p',',p')
+                value[i] = value[i].replace(new RegExp(':','g'),',')
                 value[i] = value[i].split('-');
-                value[i][0].replace('a',',a')
-                value[i][0].replace('p',',p')
-                value[i][0].replace(':',',')
-                value[i][0].split(',');
-                value[i][0] = new Date()
-
+                // value[i][0] = value[i][0].split(',');
+                console.log(value[i][1]);
+                value[i][1] = value[i][1].split(',');
+                
             }
+            
 
 
             return value;
@@ -66,10 +68,14 @@ x('http://dining.gmu.edu/dining-choices/hours-of-operation/', {
     });
 
 })
-let to_Date_Ob = function(hours){
-    if(hours[2] === 'pm'){
-        hours[1]+= 12;
-    }
-    let date = new Date();
-    
-}
+// let to_Date_Ob = function(time){
+//     if(time[2] === 'pm'){
+//         time[0]+= 12;
+//     }
+//     let date = new Date();
+//     date.setHours(time[0]);
+//     date.setMinutes(time[1]);
+//     date.setSeconds(0);
+//     date.setUTCMilliseconds(0);
+//     return date;
+// }
